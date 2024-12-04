8a02bed3d113cb38ea708471ce82f0ce8db38dd4
BhargavaNarendra97 <bhargav@traitfit.com>
2024-12-04 15:20:48 +0530

    Updated error message

--- src/main/java/com/mpsp/cc_auth_service/service/impl/AuthServiceImpl.java
+++ src/main/java/com/mpsp/cc_auth_service/service/impl/AuthServiceImpl.java
@@ -454,8 +454,13 @@ public class AuthServiceImpl implements AuthService {
       passwordHistoryRepository.saveAll(tobeSavedPasswordHistoryList);
       return new ResetPasswordByAdminResponse(failureReasons, "Password reset successfully.");
     }
-    throw new GlobalExceptionHandler.ResetPasswordException(
-        "There is no history of password for the given users. Hence cannot reset the password");
+
+    if (resetPasswordRequest.getBehalfOf().size() == 1) {
+      throw new GlobalExceptionHandler.ResetPasswordException(failureReasons.get(0));
+    } else {
+      throw new GlobalExceptionHandler.ResetPasswordException(
+          "There is no history of password for the given users. Hence cannot reset the password");
+    }
   }
 
   @Transactional
