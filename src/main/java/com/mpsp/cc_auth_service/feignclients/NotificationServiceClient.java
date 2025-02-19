3968deabfb0f1267ff2ce3b9112450544455fcb0
BhargavaNarendra97 <bhargav@traitfit.com>
2025-02-19 10:19:38 +0530

    Added source header

--- src/main/java/com/mpsp/cc_auth_service/feignclients/NotificationServiceClient.java
+++ src/main/java/com/mpsp/cc_auth_service/feignclients/NotificationServiceClient.java
@@ -1,18 +1,17 @@
 package com.mpsp.cc_auth_service.feignclients;
 
+import com.mpsp.cc_auth_service.dto.Notification;
+import feign.Headers;
 import java.util.logging.Logger;
-
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 
-import com.mpsp.cc_auth_service.dto.Notification;
-
 @FeignClient(name = "notificationServiceClient", url = "${notification.service.url}")
+@Headers("X-DRONARJUN-SOURCE: cc-auth-service")
 public interface NotificationServiceClient {
-    Logger logger = Logger.getLogger(NotificationServiceClient.class.getName());
+  Logger logger = Logger.getLogger(NotificationServiceClient.class.getName());
 
-    @PostMapping("/api/v1/notifications/send")
-    void sendNotification(
-        @RequestBody final Notification body);
+  @PostMapping("/api/v1/notifications/send")
+  void sendNotification(@RequestBody final Notification body);
 }
